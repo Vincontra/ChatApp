bb426e41ea53f5628cf158cd9a63247a84f733cb
vinay <vinaygadge2003@gmail.com>
2026-08-19 17:16:44 +0530

    Backend for chat started

---
+++ src/main/java/com/sub/chat/chat_app_backend/entities/Room.java
@@ -0,0 +1,27 @@
+package com.sub.chat.chat_app_backend.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Document(collection = "rooms")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Room {
+    @Id
+    private String id;  // mongo db: primary key matlab
+    private String roomID;
+    private List<Message>messages=new ArrayList<>();
+
+
+}
